2ade29107bc8dcf8003847db2dcee45e43d37b12
Andrew Stribblehill <stribb@gmail.com>
2019-06-19 02:54:20 +0200

    Treat direnv-non-file-modes as possibly derived modes.
    Say major-mode is magit-status-mode, which is derived from magit-mode.
    With this change, if direnv-non-file-modes contains magit-mode, direnv-mode
    will still treat it as a non-file mode.

--- direnv.el
+++ direnv.el
@@ -66,17 +66,19 @@ usually results in coloured output."
 (defcustom direnv-non-file-modes '(eshell-mode dired-mode)
   "List of modes where direnv will update even if the buffer has no file.
 
-In these modes, direnv will use `default-directory' instead of
-`(file-name-directory (buffer-file-name (current-buffer)))'."
+In these modes, or modes derived from them, direnv will use
+  `default-directory'
+instead of
+  `(file-name-directory (buffer-file-name (current-buffer)))'."
   :group 'direnv
   :type '(repeat (symbol :tag "Major mode")))
 
 (defun direnv--directory ()
   "Return the relevant directory for the current buffer, or nil."
   (let* ((buffer (or (buffer-base-buffer) (current-buffer)))
-         (file-name (buffer-file-name buffer)))
+	 (file-name (buffer-file-name buffer)))
     (cond (file-name (file-name-directory file-name))
-          ((member major-mode direnv-non-file-modes) default-directory))))
+	  ((apply #'derived-mode-p direnv-non-file-modes) default-directory))))
 
 (defun direnv--export (directory)
   "Call direnv for DIRECTORY and return the parsed result."
