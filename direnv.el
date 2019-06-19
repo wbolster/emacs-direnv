92c5d44d9b0cbc756daeee3fa4ea53e211c2581d
Andrew Stribblehill <stribb@gmail.com>
2019-06-19 09:06:01 +0200

    Untabify direnv--directory
    I'h having a hard time believing it but I had indent-tabs-mode t.

--- direnv.el
+++ direnv.el
@@ -76,9 +76,9 @@ instead of
 (defun direnv--directory ()
   "Return the relevant directory for the current buffer, or nil."
   (let* ((buffer (or (buffer-base-buffer) (current-buffer)))
-	 (file-name (buffer-file-name buffer)))
+         (file-name (buffer-file-name buffer)))
     (cond (file-name (file-name-directory file-name))
-	  ((apply #'derived-mode-p direnv-non-file-modes) default-directory))))
+          ((apply #'derived-mode-p direnv-non-file-modes) default-directory))))
 
 (defun direnv--export (directory)
   "Call direnv for DIRECTORY and return the parsed result."
